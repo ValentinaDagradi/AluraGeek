91d2f5107bba60f8baa8a55760df297be79b00fa
ValentinaDagradi <valedagradi@gmail.com>
2023-04-06 21:44:49 -0300

    cambio de ruta

--- JavaScript/controllers/mostrar-producto.js
+++ JavaScript/controllers/mostrar-producto.js
@@ -5,7 +5,7 @@ const crearNuevaCaja = (imagen, nombre, precio, id) => {
     const contenido = `<img src=${imagen} alt=${nombre} class="producto__imagen" data-imagen>
         <h3 class="producto__nombre" data-nombre>${nombre}</h3>
         <p class="producto__precio" data-precio>$${precio}</p>
-        <a href="pagina-de-producto.html?id=${id}" data-link class="producto__link"><botton class="producto__boton">Ver producto</botton></a>`;
+        <a href="./screens/pagina-de-producto.html?id=${id}" data-link class="producto__link"><botton class="producto__boton">Ver producto</botton></a>`;
     caja.classList.add("producto")
     caja.innerHTML = contenido;
     return caja;
