017eeed13693c55420c44246cb255be47374dc1c
ValentinaDagradi <valedagradi@gmail.com>
2023-04-08 15:12:47 -0300

    repositorio aluraGeek

--- JavaScript/productos.js
+++ JavaScript/productos.js
@@ -1,7 +1,7 @@
-const listaProductos = () => fetch("https://my-json-server.typicode.com/ValentinaDagradi/E-commerce/producto").then( respuesta => respuesta.json())
+const listaProductos = () => fetch("https://my-json-server.typicode.com/ValentinaDagradi/AluraGeek/producto").then( respuesta => respuesta.json())
 
 const crearProductos = (imagen, nombre, precio, tipo, descripcion) => {
-    return fetch("https://my-json-server.typicode.com/ValentinaDagradi/E-commerce/producto", {
+    return fetch("https://my-json-server.typicode.com/ValentinaDagradi/AluraGeek/producto", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -11,17 +11,17 @@ const crearProductos = (imagen, nombre, precio, tipo, descripcion) => {
 };
 
 const eliminarProducto = (id) =>{
-    return fetch(`https://my-json-server.typicode.com/ValentinaDagradi/E-commerce/producto/${id}`, {
+    return fetch(`https://my-json-server.typicode.com/ValentinaDagradi/AluraGeek/producto/${id}`, {
         method: "DELETE"
     })
 }
 
 const detalleProducto = (id) => {
-    return fetch(`https://my-json-server.typicode.com/ValentinaDagradi/E-commerce/producto/${id}`).then(respuesta  => respuesta.json())
+    return fetch(`https://my-json-server.typicode.com/ValentinaDagradi/AluraGeek/producto/${id}`).then(respuesta  => respuesta.json())
 }
 
 const actualizarProducto = (imagen, nombre, precio, tipo, descripcion,id) => {
-    return fetch(`https://my-json-server.typicode.com/ValentinaDagradi/E-commerce/producto/${id}`, {
+    return fetch(`https://my-json-server.typicode.com/ValentinaDagradi/AluraGeek/producto/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
